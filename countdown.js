077112de224d3eb64f762cdcdaeaaa394631624c
Ben Underwood <benu423@gmail.com>
2020-02-02 23:42:06 -0500

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,7 +1,7 @@
 $(function () {
 
 
-$('#clock-c').countdown('3/1/2020', function(event) {
+$('#clock-c').countdown(new Date(2020, 3, 1), function(event) {
       var $this = $(this).html(event.strftime(''
         + '<span class="h1 font-weight-bold">%D</span> Day%!d'
         + '<span class="h1 font-weight-bold">%H</span> Hr'
