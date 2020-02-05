66b1d863545712960002987244def0cb7bb20252
Ben Underwood <benu423@gmail.com>
2020-02-05 18:20:04 -0500

    Create countdown.js

---
+++ Scripts/countdown.js
@@ -0,0 +1,11 @@
+$(function () {
+
+
+$('#clock-c').countdown(new Date(2020, 3, 3,8,5), function(event) {
+      var $this = $(this).html(event.strftime(''
+        + '<span class="h1 font-weight-bold">%D</span> Day%!d'
+        + '<span class="h1 font-weight-bold">%H</span> Hours'
+        + '<span class="h1 font-weight-bold">%M</span> Minutes'
+        + '<span class="h1 font-weight-bold">%S</span> Seconds'));
+    });
+    });
